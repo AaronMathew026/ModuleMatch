c1f74e6213200897888e31b7782d4bf61125b73d
Aaron <aaron.mathew@student.manchester.ac.uk>
2026-04-12 17:55:05 +0100

    Embedding section completed

--- Backend/reasoning.py
+++ Backend/reasoning.py
@@ -1,47 +1,60 @@
+import os
+import os
 import pandas as pd
 from tqdm import tqdm
-from openai import OpenAI
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
-import os
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+
 load_dotenv(override=True)
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large", client=OpenAI())
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
-db_name = f"Backend\\Embeddings\\module_embeddings.db"
+db_name = "Backend/Embeddings/module_embeddings.db"
 
-def main():
-    embedding("Backend\\raw_csv_data\\CS_Modules.csv",)
-    
+def main(mode,input=None):
+    if mode == "embedding":
+        print("Creating Embeddings...")
+        embedding("Backend/raw_csv_data/CS_Modules.csv",)
+    elif mode == "semantic_search":
+        print("Semantic Search")
+        pass
+    elif mode == "reasoning":
+        print("Reasoning")
+        pass
 
-
-    pass
-
-
-
-
-
-def embedding(file,):
-    documents = []
+def embedding(file, test=False):
     df = pd.read_csv(file)
-    documents = [
-    {
-        "metadata": {
-            "code": course["code"],
-            "title": course["title"],
-            "year": course["year"]
-        },
-        "page_content": course["summary"]
-    }
-    for index,course in df.iterrows()
-    ]
+    documents = []
+    
+    for index, course in tqdm(df.iterrows(), total=len(df), desc="Creating Documents"):
+        doc = Document(
+            page_content=course["summary"],
+            metadata={
+                "code": course["code"],
+                "title": course["title"],
+                "year": course["year"],
+            },
+        )
+        doc.metadata["doc_type"] = "course"
+        documents.append(doc)
+        
     print(f"Created {len(documents)} documents for embedding.")
+    
     if os.path.exists(db_name):
-        os.remove(db_name)
+        Chroma(persist_directory=db_name, embedding_function=embeddings).delete_collection()
+        
+    vectorstore = Chroma.from_documents(documents, embeddings, persist_directory=db_name)
+
+    if test:
+
+        collection = vectorstore._collection
+        count = collection.count()
+
+        sample_embedding = collection.get(limit=1, include=["embeddings"])["embeddings"][0]
+        dimensions = len(sample_embedding)
+        print(f"There are {count:,} vectors with {dimensions:,} dimensions in the vector store")
 
 
 if __name__ == "__main__":
-    main()
-
-
-
-
+    main("semantic_search")
